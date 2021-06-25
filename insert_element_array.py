3d56655f450537223acc817a711db1c89ccaed09
Itnesh Kumar <74128420+Itnesh@users.noreply.github.com>
2021-06-25 20:44:40 +0530

    Insert the element in array
    How to insert the element in the array

---
+++ insert_element_array.py
@@ -0,0 +1,10 @@
+from array import *
+arr=array('i',[1,3,5,6,3,6]) # i repersent the intger element
+print(arr)
+arr1=array('d',[1.3,2.4,3.6]) #d represent the float type element
+print(arr1)
+
+arr.insert(1,8)
+print(arr)
+arr.insert(0,4)
+print(arr)
